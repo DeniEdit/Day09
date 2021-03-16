8d22bec6d95b519051751b938e3a0ca8187a2eaf
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 12:58:37 +0100

    Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myFunction(myParameter) {
+    if (myParameter) {
+        return "The parameter is true!";
+    }
+    return "The parameter is false!";
+}
+console.log("The parameter is true!");
+console.log("The parameter is false!");
+// Only change code below this line
+myFunction("The parameter is true!");
+myFunction("The parameter is false!");
+module.exports = myFunction;
