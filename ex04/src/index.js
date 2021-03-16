399e5d4f3f35967804955b7821c02543c1c75e74
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 13:25:50 +0100

    Exercise 04

---
+++ ex04/src/index.js
@@ -0,0 +1,23 @@
+// Only change code below this line
+function compareDifferentValues(m, n) {
+    if (m === n) {
+        return "Equal";
+    } else{
+    return "Not equal";
+
+    }
+}
+console.log("Not equal");
+console.log("Not equal");
+console.log("Not equal");
+console.log("Equal");
+console.log("Equal");
+// Only change code below this line
+compareDifferentValues(8, "8");
+compareDifferentValues("8", 8);
+compareDifferentValues('8', 8);
+compareDifferentValues("8", "8");
+compareDifferentValues(8,8);
+
+
+module.exports = compareDifferentValues;
