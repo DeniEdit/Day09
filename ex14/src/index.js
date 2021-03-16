f03120662915bcb07c25687ab2c1efa848c62abf
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 17:58:01 +0100

    Exercise 14

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myForLoop1(){
+    for (var evenNumbers = 0; evenNumbers < 10; evenNumbers++){
+        if (evenNumbers % 2 === 0)
+        console.log(evenNumbers); 
+    }
+}
+
+console.log(myForLoop1());
+// Only change code below this line
+module.exports = {
+    myForLoop1,
+    myForLoop2
+};
