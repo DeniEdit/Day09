0c1f9556d6608ce606b82ab4b0c6470ed09183cc
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 14:23:30 +0100

    Exercise 05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,22 +1,17 @@
 // Only change code below this line
 function compareStrictValues(m) {
     if (m != 55) {
+        return "Not equal";
+    } else {
         return "Equal";
-    } else{
-    return "Not equal";
 
     }
 }
-console.log("Equal");
-console.log("Equal");
-console.log("Not equal");
-console.log("Not equal");
-console.log("Not equal");
+console.log(compareStrictValues(55));
+console.log(compareStrictValues("55"));
+console.log(compareStrictValues("21"));
+console.log(compareStrictValues(12));
+console.log(compareStrictValues("Doe"));
 // Only change code below this line
-compareStrictValues(55);
-compareStrictValues("55");
-compareStrictValues("21");
-compareStrictValues(12);
-compareStrictValues("Doe");
 module.exports = compareStrictValues;
 
