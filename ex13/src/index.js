00b616aa40ddcc1b5e430a872e67739fa12d88f2
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 19:16:29 +0100

    Exercise 12

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function inverseWhile(){
+    var fiveNumbers = 0;
+    while(fiveNumbers <= 5){
+        console.log(fiveNumbers);
+        fiveNumbers++;
+    }
+}
+// Only change code below this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
