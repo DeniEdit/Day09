42fbed06f711efeb55e647d9b980e504f79f896b
DeniEdit <milicevic.deni@gmail.com>
2021-03-16 13:17:42 +0100

    Exercise 03

---
+++ ex03/src/index.js
@@ -0,0 +1,23 @@
+// Only change code below this line
+function myEqualFunction(num) {
+    if (num === 23) {
+        return "Equal";
+    } else{
+    return "Not equal";
+
+    }
+}
+console.log("Not equal");
+console.log("Equal");
+console.log("Not equal");
+console.log("Not equal");
+console.log("Not equal");
+// Only change code below this line
+myEqualFunction(5);
+myEqualFunction(23);
+myEqualFunction("23");
+myEqualFunction('23');
+myEqualFunction("text");
+
+
+module.exports = myEqualFunction;
